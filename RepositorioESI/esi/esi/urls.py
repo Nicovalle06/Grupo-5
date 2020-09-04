add21ed4cb542f3511a0ebee3196184469a463c3
caballo1986 <cesaro.aquino@gmail.com>
2020-09-04 11:25:22 -0300

    se agrega noticias y foro

--- RepositorioESI/esi/esi/urls.py
+++ RepositorioESI/esi/esi/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Home,name="home"),
     path('home/',views.Home,name="home"),
+    path('noticias/',views.Noticias,name="noticias"),
+    path('foro/',views.Foro,name="foro"),
+
 ]
--- RepositorioESI/esi/esi/views.py
+++ RepositorioESI/esi/esi/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 
 def Home(request):
-	print("HOLA")
 	return render(request,'home.html')
+def Noticias(request):
+	return render(request,'noticias.html')
+def Foro(request):
+	return render(request,'foro.html')
