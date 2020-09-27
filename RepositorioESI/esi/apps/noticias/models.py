84e7f049e3689224628fddefc95f62ed6a57427b
caballo1986 <cesaro.aquino@gmail.com>
2020-09-26 23:03:41 -0300

    noticias v3.0

--- RepositorioESI/esi/apps/noticias/models.py
+++ RepositorioESI/esi/apps/noticias/models.py
@@ -3,18 +3,28 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from django.contrib import admin
+
+from datetime import datetime
+from django.utils.functional import lazy
+from django.utils.timezone import localtime, now
 # Create your models here.
 
 
 
 class New(models.Model): 
-	titulo = models.CharField(max_length=200)
-	autor = models.CharField(max_length=200)
-	copete = models.TextField()
-	cuerpo = models.TextField()
-	imagen = models.ImageField()
-	fecha_publicacion = models.DateTimeField(default=timezone.now)
-	fuente = models.CharField(max_length=400)
+	titulo = models.CharField('titulo de la noticia',max_length=200)
+	autor = models.CharField('autor',max_length=200)
+	copete = models.TextField('copete')
+	cuerpo = models.TextField('texo')
+	imagen = models.ImageField('imagen')
+	fecha_publicacion = models.DateTimeField('fecha de la publicacion',editable=True, auto_now_add=True, auto_now=False)
+	fuente = models.URLField('fuente URL',max_length=400)
 
 	def __str__(self):
 		return self.titulo
+	"""@classmethod
+				def fecha(cls):
+					return self.fecha_publicacion.strftime("%m/%d/%Y)"""
+	"""@property
+			    def fechas(self):
+			        return self.fecha_publicacion.strftime('%d %m %Y')"""
